70ad6d209bd2d3516f938702c9cb296a05df687c
조지형 <jihyoung0103@gmail.com>
2025-03-22 10:58:48 +0900

    setting

---
+++ backend/src/main/java/com/example/SMU_Wordmaster/SmuWordmasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.SMU_Wordmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmuWordmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmuWordmasterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/SMU_Wordmaster/SmuWordmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SMU_Wordmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmuWordmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
